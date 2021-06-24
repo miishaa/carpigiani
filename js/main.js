cc9b2adb9101d9f0f2cfb491bbd5e609d3649761
miishaa <supermikhailo@gmail.com>
2021-06-24 08:44:23 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new CircleType(document.getElementById('up'))
+    .radius(200);
+
+new CircleType(document.getElementById('up-2'))
+    .radius(200);
+
+new CircleType(document.getElementById('down'))
+    .dir(-1)
+    .radius(200);

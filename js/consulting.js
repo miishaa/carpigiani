0eb59a44b66ee6e1e7773aeb579946894313e01a
miishaa <supermikhailo@gmail.com>
2021-07-14 18:37:14 +0300

    add form

---
+++ js/consulting.js
@@ -0,0 +1,65 @@
+// trick
+
+function animateMarquee(el, duration) {
+    const innerEl = el.querySelector('.marquee__inner');
+    const innerWidth = innerEl.offsetWidth;
+    const cloneEl = innerEl.cloneNode(true);
+    el.appendChild(cloneEl);
+
+    let start = performance.now();
+    let progress;
+    let translateX;
+
+    requestAnimationFrame(function step(now) {
+        progress = (now - start) / duration;
+
+        if (progress > 1) {
+            progress %= 1;
+            start = now;
+        }
+
+        translateX = innerWidth * progress;
+
+        innerEl.style.transform = `translate3d(-${translateX}px, 0 , 0)`;
+        cloneEl.style.transform = `translate3d(-${translateX}px, 0 , 0)`;
+        requestAnimationFrame(step);
+    });
+}
+const marquee5 = document.querySelector('#marquee5');
+
+
+animateMarquee(marquee5, 10000);
+
+
+// Wait for the DOM to be ready
+$(function() {
+    // Initialize form validation on the registration form.
+    // It has the name attribute "registration"
+    $("form").validate({
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            firstname: "required",
+            lastname: "required",
+            email: {
+                required: true,
+                // Specify that email should be validated
+                // by the built-in "email" rule
+                email: true
+            }
+        },
+        // Specify validation error messages
+        messages: {
+            firstname: "Please enter your firstname",
+            lastname: "Please fuckoff",
+            email: "Please enter a valid email address"
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+});
--- js/main.js
+++ js/main.js
@@ -198,25 +198,27 @@ jQuery(document).ready(function(){
 
 // test
 // вкладки с содержанием
+//
+// $(".tab_content").hide();
+// $(".tab_content:first").show();
+// /* в режиме вкладок */
+// $("ul.tabs li").click(function () {
+//     $(".tab_content").hide();
+//     var activeTab = $(this).attr("rel");
+//     $("#" + activeTab).fadeIn();
+//     $("ul.tabs li").removeClass("active");
+//     $(this).addClass("active");
+//     $(".tab_accordion").removeClass("d_active");
+//     $(".tab_accordion[rel^='" + activeTab + "']").addClass("d_active");
+// });
+// /* в режиме аккордеона */
+// $(".tab_accordion").click( function () {
+//     $(".tab_content").hide();
+//     var d_activeTab = $(this).attr("rel");
+//     $("#" + d_activeTab).fadeIn();
+//     $(".tab_accordion").removeClass("d_active");
+//     $("ul.tabs li").removeClass("active");
+//     $("ul.tabs li[rel^='" + d_activeTab + "']").addClass("active");
+// });
 
-$(".tab_content").hide();
-$(".tab_content:first").show();
-/* в режиме вкладок */
-$("ul.tabs li").click(function () {
-    $(".tab_content").hide();
-    var activeTab = $(this).attr("rel");
-    $("#" + activeTab).fadeIn();
-    $("ul.tabs li").removeClass("active");
-    $(this).addClass("active");
-    $(".tab_accordion").removeClass("d_active");
-    $(".tab_accordion[rel^='" + activeTab + "']").addClass("d_active");
-});
-/* в режиме аккордеона */
-$(".tab_accordion").click( function () {
-    $(".tab_content").hide();
-    var d_activeTab = $(this).attr("rel");
-    $("#" + d_activeTab).fadeIn();
-    $(".tab_accordion").removeClass("d_active");
-    $("ul.tabs li").removeClass("active");
-    $("ul.tabs li[rel^='" + d_activeTab + "']").addClass("active");
-});
+
